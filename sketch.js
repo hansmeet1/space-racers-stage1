a53e4e6294e411446644b7187882666b6f6d1bb6
hansmeet1 <95033543+hansmeet1@users.noreply.github.com>
2022-08-22 18:52:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+var r1_img,r2_img,m1_img,m2_img,m3_img,bg_img,fuel_img
+function preload(){
+  r1_img=loadImage("assets/rocket1.jpg")
+  r2_img=loadImage("assets/rocket2.jpg")
+  m1_img=loadImage("assets/meteor1.png")
+  m2_img=loadImage("assets/meteor2.jpg")
+  m3_img=loadImage("assets/meteor3.jpg")
+  bg_img=loadImage("assets/space back ground.jpg")
+  fuel_img=loadImage("assets/fuel.png")
+}
